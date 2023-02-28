0928a8e2a8697cdf7d73af6177db3cb882699a35
Mary Rohrdanz <marohrdanz@mdanderson.org>
2023-02-28 10:15:38 -0600

    Payload has what we need?

--- index.js
+++ index.js
@@ -81,7 +81,9 @@ function main() {
     core.debug(`Tag prefix: ${prefix}`);
     core.debug("GITHUB_BASE_REF:", process.env.GITHUB_BASE_REF);
     core.debug("GITHUB_REPOSITORY:", process.env.GITHUB_REPOSITORY);
-    core.debug(`GITHUB_REPOSITORY_OWNER: ${GITHUB_REPOSITORY_OWNER}`);
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+    //core.debug(`GITHUB_REPOSITORY_OWNER: ${GITHUB_REPOSITORY_OWNER}`);
 
     let nextBuildNumber;
     /* 
