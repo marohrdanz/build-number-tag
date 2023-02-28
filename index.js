46269c4d942e2f2ed39cad7008b2513cd7551d89
Mary Rohrdanz <marohrdanz@mdanderson.org>
2023-02-28 14:12:44 -0600

    Revert "Working?"
    This reverts commit 248dbe28f40c54012e58df7d5899c40cbd71f07f.

--- index.js
+++ index.js
@@ -87,10 +87,8 @@ function main() {
       const payload = JSON.stringify(github.context.payload, undefined, 2)
       console.log(`The event payload: ${payload}`);
       //core.debug(`Repo name: ", ${payload.repository.name}`);
-      const repo_name = github.context.payload.repository.name;
+      const repo_name = github.context.payload.repository.owner.name;
       core.debug(`Repo name: ${repo_name}`);
-      core.debug("Repo owner: ", github.context.payload.repository.owner.name);
-
       //core.debug(`GITHUB_REPOSITORY_OWNER: ${GITHUB_REPOSITORY_OWNER}`);
     } catch(err) {
       core.error(err);
