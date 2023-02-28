b7ea9904c08f617a8e403259441525f815aefb36
Mary Rohrdanz <marohrdanz@mdanderson.org>
2023-02-28 12:41:52 -0600

    Do more refactoring

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-const https = require('https');
-const zlib = require('zlib');
 const core = require('@actions/core');
 const github = require('@actions/github');
 
@@ -7,6 +5,8 @@ const myToken = core.getInput('token');
 const octokit = github.getOctokit(myToken);
 const repo_name = github.context.payload.repository.name;
 const repo_owner = github.context.payload.repository.owner.name;
+//const prefix = core.getInput('prefix');
+//core.debug(`Tag prefix: ${prefix}`);
 
 main();
 
@@ -23,34 +23,35 @@ async function getAllTags() {
   return(allTags);
 }
 
-function getTagsMatchingPrefix(tags) {
-  const prefix = core.getInput('prefix');
-  core.debug(`Tag prefix: ${prefix}`);
+function getTagsMatchingPrefix(tags, prefix) {
   const regexString = `${prefix}(\\d+)$`;
   const regex = new RegExp(regexString);
   let tagsMatchingPrefix = tags.filter(t => t.name.match(regex));
   return (tagsMatchingPrefix);
 }
 
+function getBuildNumber(tags, prefix) {
+  let build_number;
+  if (tags.length < 1) {
+    core.info(`No tags matching ${prefix}. Setting build_number to 1.`);
+    build_number = 1;
+    return (build_number);
+  }
+  let existingBuildNumbers = tags.map(t => parseInt(t.name.match(/-(\d+)$/)[1]));
+  core.debug("Existing build numbers: ", JSON.stringify(existingBuildNumbers, undefined, 2));
+  let currentBuildNumber = Math.max(...existingBuildNumbers);
+  core.info(`Largest '${prefix}' tag is ${currentBuildNumber}.`);
+  build_number = currentBuildNumber + 1;
+  return (build_number);
+}
 
 async function main() {
-    //const prefix = `${env.INPUT_PREFIX}`; // default specified in action.yml
     try {
       let allTags = await getAllTags();
-      let tagsMatchingPrefix = getTagsMatchingPrefix(allTags);
-      let build_number;
-      if (tagsMatchingPrefix.length < 1) {
-        build_number = 0;
-      } else {
-        //core.debug("Tags matching prefix: ");
-        //core.debug(JSON.stringify(tagsMatchingPrefix, undefined, 2));
-        let existingBuildNumbers = tagsMatchingPrefix.map(t => parseInt(t.name.match(/-(\d+)$/)[1]));
-        core.debug("Existing build numbers: ", JSON.stringify(existingBuildNumbers, undefined, 2));
-        let currentBuildNumber = Math.max(...existingBuildNumbers);
-        core.info(`Largest '${prefix}' tag is ${currentBuildNumber}.`);
-        build_number = currentBuildNumber + 1;
-        core.info(`Updating '${prefix}' counter to ${build_number}.`);
-      }
+      let tagsMatchingPrefix = getTagsMatchingPrefix(allTags, core.getInput('prefix'));
+      let build_number = getBuildNumber(tagsMatchingPrefix, core.getInput('prefix'));
+      core.info(`New build number for tag: ${build_number}.`);
+      core.setOutput("build_number", build_number);
     } catch(err) {
       core.error(err);
       core.setFailed(err);
