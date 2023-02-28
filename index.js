248dbe28f40c54012e58df7d5899c40cbd71f07f
Mary Rohrdanz <marohrdanz@mdanderson.org>
2023-02-28 10:29:05 -0600

    Working?

--- index.js
+++ index.js
@@ -87,8 +87,10 @@ function main() {
       const payload = JSON.stringify(github.context.payload, undefined, 2)
       console.log(`The event payload: ${payload}`);
       //core.debug(`Repo name: ", ${payload.repository.name}`);
-      const repo_name = github.context.payload.repository.owner.name;
+      const repo_name = github.context.payload.repository.name;
       core.debug(`Repo name: ${repo_name}`);
+      core.debug("Repo owner: ", github.context.payload.repository.owner.name);
+
       //core.debug(`GITHUB_REPOSITORY_OWNER: ${GITHUB_REPOSITORY_OWNER}`);
     } catch(err) {
       core.error(err);
